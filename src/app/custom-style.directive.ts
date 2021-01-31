8d37af4037cec71e121218f4d4d4c17f45df2882
Sebghat Yusuf <sebghatyusuf@gmail.com>
2021-01-31 14:14:04 +0430

    finish directives

--- src/app/custom-style.directive.ts
+++ src/app/custom-style.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appCustomStyle]'
 })
 export class CustomStyleDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { 
+    el.nativeElement.style.color = 'blue'
+  }
+
+
 
 }
