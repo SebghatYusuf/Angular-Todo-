abd957b18586b949139aead3e809398c45e39b12
Sebghat Yusuf <sebghatyusuf@gmail.com>
2021-02-03 08:59:55 +0430

    add search

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -1,3 +1,4 @@
+import { apiKey } from './creds';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -14,14 +15,14 @@ export class AppServiceService {
     constructor(private http: HttpClient) { }
 
     getImages() {
-        return this.http.get('https://pixabay.com/api/?key=20022653-0b92ec61ee2185327ea8a9977&q=sex&image_type=photo&safesearch=true');
+        return this.http.get(`https://pixabay.com/api/?key=${apiKey}&q=nature&image_type=photo&safesearch=true`);
 
     }
 
     search(text: string) {
 
         console.log('searching for',);
-        var url = `https://pixabay.com/api/?key=20022653-0b92ec61ee2185327ea8a9977&q=${text}&image_type=photo&per_page=50`;
+        var url = `https://pixabay.com/api/?key=${apiKey}&q=${text}&image_type=photo&per_page=50`;
         console.log(url)
         return this.http.get(url);
 
