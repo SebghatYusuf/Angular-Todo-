4510711016ce1922dfb2bc1dfc9d4cda18e130a4
Sebghat Yusuf <sebghatyusuf@gmail.com>
2021-02-10 09:15:52 +0430

    add tailwind and fa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,7 +73,7 @@ export class AppComponent {
   //   console.warn(this.loginForm.value )
   // }
   constructor(private appservice: AppServiceService) {
-
+    this.loadImages();
   }
 
   async loadImages() {
